4d29025f9be4a94624970ddf45d5f2cba325ec83
Akira Morales <akira.k.morales@gmail.com>
2018-09-17 13:36:39 -0400

    Add files via upload

---
+++ Simple Calculator/src/CalculatorMain.java
@@ -0,0 +1,13 @@
+
+public class CalculatorMain
+{
+
+	public static void main(String[] args)
+	{
+		//This is how we create an object
+		// new is a java key word which creates an object
+		Calculator calculator = new Calculator();
+		System.out.print(calculator.add(2, 6));
+		
+	}
+}
