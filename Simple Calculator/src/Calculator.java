c4e3cd04dcdec4fb608a0855f193bb608a88f733
AKIRA <AKIRA@AKIRA-PC.fios-router.home>
2018-09-17 22:02:00 -0400

    Created the subtract method.  Finished assignment.

--- Simple Calculator/src/Calculator.java
+++ Simple Calculator/src/Calculator.java
@@ -3,10 +3,17 @@ public class Calculator
 {
 	
 	public int add(int num1, int num2)
+	
 	{
 		int ans = num1 + num2;
 		return ans;
+		
 	}
 
-	
+	public int subt(int num1, int num2)
+	{
+		
+		int ans = num1 - num2;
+		return ans;
+	}
 }
--- Simple Calculator/src/CalculatorMain.java
+++ Simple Calculator/src/CalculatorMain.java
@@ -7,7 +7,7 @@ public class CalculatorMain
 		//This is how we create an object
 		// new is a java key word which creates an object
 		Calculator calculator = new Calculator();
-		System.out.print(calculator.add(2, 6));
-		
+		System.out.println("The sum of the two numbers is " + calculator.add(2, 6));
+		System.out.println("The difference of the two numbers is " + calculator.subt(5,4));
 	}
 }
